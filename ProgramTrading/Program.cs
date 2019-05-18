f08f82757a716cfe9aed33ef982d305d2eaaeee7
HUNTER <HUNTER@DESKTOP-U017C42>
2019-05-18 10:35:53 +0800

    update

--- ProgramTrading/Program.cs
+++ ProgramTrading/Program.cs
@@ -12,7 +12,7 @@ namespace Lab2
         static MrWangConnection MrWangConnection;
         static void Main(string[] args)
         {
-            //123//456
+            //123//456//789
             //初始化API元件
             init();
             //執行連線
